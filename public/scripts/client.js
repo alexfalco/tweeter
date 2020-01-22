296d98c762983ab16f3ae9160073d4bbb0592936
alexfalco <afalconer02@gmail.com>
2020-01-22 09:32:25 -0500

    fixed tweet time

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,99 +4,82 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
+const loadTweets = function() {
+  $.ajax({
+    url: "/tweets",
+    method: "GET"
+    //dataType: JSON ,
+  }).done(function(result) {
+    renderTweets(result);
+  });
+};
+
 $(document).ready(function() {
   loadTweets();
 });
 
-
-
-const  loadTweets = function() {
-  $.ajax({
-    url: '/tweets',
-    method: "GET",
-    //dataType: JSON ,
-  })
-    .done(function(result) {
-      renderTweets(result);
-    }
-    );
-
-};
-
-
 const createTweetElement = function(tweetObj) {
+  let $tweet = $("<article>");
+  let $header = $("<header>");
+  const $avatar = $("<img>").attr("src", tweetObj.user.avatars);
+  const $username = $("<p>").text(tweetObj.user.name);
+  const $handle = $("<p>").text(tweetObj.user.handle);
+  const $content = $("<p>").text(tweetObj.content.text);
+  const $line = $("<hr>");
+  const $footer = $("<footer>").text(moment(tweetObj.created_at).fromNow());
 
-  let $tweet = $('<article>');
-  let $header = $('<header>');
-  let $avatar = $('<img>').attr('src',tweetObj.user.avatars);
-  let $username = $('<p>').text(tweetObj.user.name);
-  let $handle = $('<p>').text(tweetObj.user.handle);
-  let $content = $('<p>').text(tweetObj.content.text);
-  let $line = $('<hr>');
-  let $footer = $('<footer>').text(tweetObj.created_at);
-  
   $header = $header
     .append($avatar)
     .append($username)
     .append($handle);
-   
-  $tweet = $tweet.append($header).append($content).append($line).append($footer);
+
+  $tweet = $tweet
+    .append($header)
+    .append($content)
+    .append($line)
+    .append($footer);
 
   $("#tweetContainer").prepend($tweet);
- 
 };
 
-
 const renderTweets = function(arrayTweet) {
-
   for (let tweet of arrayTweet) {
     createTweetElement(tweet);
   }
-
 };
 
-
-// Submit a Tweet
 $(document).ready(function() {
- 
-  $('.new-tweet form').on('submit', function(event) {
-  
-    
+  // Unhide the tweet section
+  $(".new-tweet").hide();
+  $("#Bar2").click(function() {
+    $(".new-tweet").toggle();
+  });
+
+  // Unhide the tweet section
+  $(".new-tweet").hide();
+  $("#Bar4").click(function() {
+    $(".new-tweet").toggle();
+  });
+
+  // Submit a Tweet
+  $(".new-tweet form").on("submit", function(event) {
     event.preventDefault();
-   
-    if ($('#text').val() === "") {
-      $("#erroMsg").text('You need to type something');
-    } else if ($('#text').val().length >= 14) {
-      $("#erroMsg").text('140 is the Max buddy');
+
+    if ($("#text").val() === "") {
+      $("#erroMsg").text("You need to type something");
+    } else if ($("#text").val().length >= 140) {
+      $("#erroMsg").text("140 is the Max buddy");
     } else {
-      $("#erroMsg").text('');
+      $("#erroMsg").text("");
       $.ajax({
-        url: '/tweets',
+        url: "/tweets",
         method: "POST",
-        data: $('#text').serialize(),
-      })
-        .done(function(result) {
-          loadTweets(result);
-        });
+        data: $("#text").serialize()
+      }).done(function(result) {
+        loadTweets(result);
+        //$("#text").text("");  Need to Clear the form after submit the tweet
+        //.reset(".new-tweet form");
+      });
     }
-  });
-
-});
-
-
-// Unhide the tweet section
-$(document).ready(function() {
-  $('.new-tweet').hide();
-  $('#Bar2').click(function() {
-    $('.new-tweet').toggle();
-  });
-});
-
-
-// Unhide the tweet section
-$(document).ready(function() {
-  $('.new-tweet').hide();
-  $('#Bar4').click(function() {
-    $('.new-tweet').toggle();
   });
 });
